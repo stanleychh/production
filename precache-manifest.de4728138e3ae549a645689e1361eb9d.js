6deb77ee63f690f502500a37f86fa8b177eed7e4
Stanley Hsu <ching.hsiang.hsu@sap.com>
2020-12-17 16:49:12 +0100

    Updates

---
+++ precache-manifest.de4728138e3ae549a645689e1361eb9d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329185c0a396c89c73a93ddd5d3cfe54",
+    "url": "/production/index.html"
+  },
+  {
+    "url": "/production/luigi-core/0072d92bfeaf57f02de04342d06a3463.woff2"
+  },
+  {
+    "url": "/production/luigi-core/1e678655fe64a8a126dd51619f22edde.woff"
+  },
+  {
+    "url": "/production/luigi-core/33bdbe44954b2ca9b5e7bbc0d5ab8860.woff"
+  },
+  {
+    "url": "/production/luigi-core/48cb3e07e7e509d6c81c420507d7916b.woff"
+  },
+  {
+    "url": "/production/luigi-core/5d817eee24266da78f96ef5e36c9019a.woff"
+  },
+  {
+    "url": "/production/luigi-core/85e09d643e5d4b5f98c3aaf365bd1c02.woff2"
+  },
+  {
+    "url": "/production/luigi-core/8b3b97ff8e0a414140609055fced4d8e.woff2"
+  },
+  {
+    "revision": "e1260bf3ad76be6ab1c30ac39aaffabf",
+    "url": "/production/luigi-core/README.md"
+  },
+  {
+    "url": "/production/luigi-core/f6226f10e41be591218339108cc242a1.woff2"
+  },
+  {
+    "revision": "07a83ef28dbcc5866b841094057e7150",
+    "url": "/production/luigi-core/luigi.css"
+  },
+  {
+    "revision": "ab48fa4759ad3bbe97fb7e43aa6a5e65",
+    "url": "/production/luigi-core/luigi.js"
+  },
+  {
+    "revision": "00a498a86bc4618639a00df0c368a99b",
+    "url": "/production/luigi-core/package.json"
+  },
+  {
+    "revision": "7f7c096e397dca4917a0600d6cb67bc0",
+    "url": "/production/sampleapp.html"
+  },
+  {
+    "revision": "d00e9a85210461325cb0",
+    "url": "/production/static/css/2.e2a9e869.chunk.css"
+  },
+  {
+    "revision": "4433b89b46bb88e20bb9",
+    "url": "/production/static/css/main.53971960.chunk.css"
+  },
+  {
+    "revision": "d00e9a85210461325cb0",
+    "url": "/production/static/js/2.0533f226.chunk.js"
+  },
+  {
+    "revision": "4433b89b46bb88e20bb9",
+    "url": "/production/static/js/main.3ad4f64f.chunk.js"
+  },
+  {
+    "revision": "4670f3d675511c7dd158",
+    "url": "/production/static/js/runtime-main.0cbfac85.js"
+  }
+]);
